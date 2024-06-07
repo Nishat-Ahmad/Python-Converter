668cde5a6b9063a3be837d65e537673ba33940a8
Assassin-Droid210 <nishat.ahmed1200@gmail.com>
2024-06-07 16:21:34 +0500

    Error 2

--- LeetCode/13. Roman to Integer/something.cpp
+++ LeetCode/13. Roman to Integer/something.cpp
@@ -2,6 +2,38 @@
 
 using namespace std;
 
-int main(){
+int romanToInt(string s) {
+    int total = 0;
+    int prevValue = 0;
     
+    for (char current : s) {
+        int currValue = value(current);
+        
+        if (currValue > prevValue) {
+            total += currValue - 2 * prevValue; // Subtract the previously added smaller value
+        } else {
+            total += currValue;
+        }
+        
+        prevValue = currValue;
+    }
+    
+    return total;
+}
+
+int value(char r) {
+    switch (r) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int main(){
+    cout << romanToInt("XXVII");
 }
---
+++ LeetCode/648. Replace words/something.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string replaceWords(vector<string>& dictionary, string sentence) {
+    string s;
+     while (getline(sentence, s, ' ')) {
+ 
+        // store token string in the vector
+        v.push_back(s);
+    }
+    for(int i = 0;i < )
+}
